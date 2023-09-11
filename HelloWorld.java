e19acf8412d8317ad632ec92c947f1d73ab1ad01
Bhagwat <darshan.bhagwat@Weatherford.com>
2023-09-11 12:17:15 +0530

    name_ipaddr

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,17 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class GetMachineNameAndIPAddress {
+
+    public static void main(String[] args) throws UnknownHostException {
+        // Get the machine name
+        String machineName = InetAddress.getLocalHost().getHostName();
+
+        // Get the machine IP address
+        String machineIPAddress = InetAddress.getLocalHost().getHostAddress();
+
+        // Print the machine name and IP address
+        System.out.println("Machine name: " + machineName);
+        System.out.println("Machine IP address: " + machineIPAddress);
+    }
 }
